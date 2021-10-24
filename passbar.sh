4d6e8d0eb490ce0a33f77db5c29dd4292f627df9
TISHA420 <89423319+TISHA420@users.noreply.github.com>
2021-10-24 13:13:39 +0600

    Add files via upload

---
+++ passbar.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+## i have used `tput civis`  for hide cursor and for normal state `tupt cnorm`
+## Note: for "tput" function first you should install **ncurses-utils** package.. then you can use the code..
+
+spin () {
+
+local pid=$!
+local delay=0.25
+local spinner=( '\033[1;32m█■■■■' '\033[1;31m■█■■■' '\033[1;32m■■█■■' '\033[1;31m■■■█■' '\033[1;32m■■■■█' )
+
+while [ "$(ps a | awk '{print $1}' | grep $pid)" ]; do
+
+for i in "${spinner[@]}"
+do
+        tput civis
+        echo -ne "\033[34m\r[*] please wait••••••••••••\e[33m[\033[32m$i\033[33m]\033[0m   ";
+        sleep $delay
+        printf "\b\b\b\b\b\b\b\b";
+done
+done
+printf "   \b\b\b\b\b"
+tput cnorm
+printf "\e[1;33m [Done]\e[0m";
+echo "";
+
+}
+( sleep 10 ) &> /dev/null & spin
